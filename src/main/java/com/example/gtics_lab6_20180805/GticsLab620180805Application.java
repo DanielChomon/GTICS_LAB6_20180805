61f7ed8bb16e1e56059c38197362359c7f7a7a07
Daniel <a20180805@pucp.edu.pe>
2023-09-21 20:34:21 -0500

    base

---
+++ src/main/java/com/example/gtics_lab6_20180805/GticsLab620180805Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab6_20180805;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab620180805Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GticsLab620180805Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gtics_lab6_20180805/GticsLab620180805ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab6_20180805;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab620180805ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
